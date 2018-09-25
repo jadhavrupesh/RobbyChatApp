0c22ac5a4c2ff4eb6450933835b2ac9e0b917ca5
Rupesh <jadhavrupesh22@gmail.com>
2018-09-25 22:43:03 +0530

    new ones

--- app/src/main/java/com/jadhavrupesh22/robbychatapp/SettingsActivity.java
+++ app/src/main/java/com/jadhavrupesh22/robbychatapp/SettingsActivity.java
@@ -1,13 +1,94 @@
 package com.jadhavrupesh22.robbychatapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.util.Random;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private DatabaseReference mUserDatabase;
+    private FirebaseUser mCurrentuser;
+
+    private CircleImageView mDisplayImage;
+    private TextView mName;
+    private TextView mStatus;
+
+    private final static int GALLARY_PICK = 1;
+
+    //Firebase Storage
+    private StorageReference mImageStorage;
+
+    //Progress
+    private ProgressDialog mProgressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        mDisplayImage = (CircleImageView) findViewById(R.id.settings_images);
+        mName = (TextView) findViewById(R.id.settings_name);
+        mStatus = (TextView) findViewById(R.id.settings_status);
+
+        mImageStorage = FirebaseStorage.getInstance().getReference();
+
+
+        mCurrentuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        String current_uid = mCurrentuser.getUid();
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(current_uid);
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String name = dataSnapshot.child("name").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+                String thumb_image = dataSnapshot.child("thumb_image").getValue().toString();
+
+                mName.setText(name);
+                mStatus.setText(status);
+
+                Picasso.get().load(image).into(mDisplayImage);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
     }
 }
