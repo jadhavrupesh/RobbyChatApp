a4f14f83f87c56455b0f3eee5350624a4ebf2858
Rupesh <jadhavrupesh22@gmail.com>
2018-09-25 21:32:13 +0530

    new one

--- app/src/main/java/com/jadhavrupesh22/robbychatapp/RegisterActivity.java
+++ app/src/main/java/com/jadhavrupesh22/robbychatapp/RegisterActivity.java
@@ -1,13 +1,122 @@
 package com.jadhavrupesh22.robbychatapp;
 
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+    //Firebase
+    private FirebaseAuth mAuth;
+
+    //Toolbar
+    private android.support.v7.widget.Toolbar mToolbar;
+
+    private TextInputLayout mDisplayName;
+    private TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+
+    //progressBas
+    private ProgressDialog mRegProgress;
+
+    //Firebase Database
+    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Toolbar
+        mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.register_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Registration");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Firebase
+        mAuth = FirebaseAuth.getInstance();
+
+        mDisplayName = (TextInputLayout) findViewById(R.id.reg_display_name);
+        mEmail = (TextInputLayout) findViewById(R.id.login_email);
+        mPassword = (TextInputLayout) findViewById(R.id.login_password);
+
+        //Progress
+        mRegProgress = new ProgressDialog(this);
+
     }
+
+
+    public void rcb(View view) {
+
+        String display_name = mDisplayName.getEditText().getText().toString();
+        String email = mEmail.getEditText().getText().toString();
+        String password = mPassword.getEditText().getText().toString();
+
+
+        if (!TextUtils.isEmpty(display_name) || !TextUtils.isEmpty(email) || !TextUtils.isEmpty(password)) {
+            mRegProgress.setTitle("Registration.");
+            mRegProgress.setMessage("Please Wait while processing......");
+            mRegProgress.setCanceledOnTouchOutside(false);
+            mRegProgress.show();
+
+
+            register_account(display_name, email, password);
+        } else {
+            Toast.makeText(RegisterActivity.this, "Please Fill All Details.... ", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    private void register_account(final String display_name, String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                    String uid = current_user.getUid();
+                    mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+                    HashMap<String, String> userMap = new HashMap<>();
+                    userMap.put("name", display_name);
+                    userMap.put("status", "hii there, i m Using Robby chat");
+                    userMap.put("image", "default");
+                    userMap.put("thumb_image", "default");
+                    mDatabase.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                mRegProgress.dismiss();
+                                Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                                startActivity(mainIntent);
+                                finish();
+                            }
+                        }
+                    });
+                } else {
+                    mRegProgress.hide();
+                    Toast.makeText(RegisterActivity.this, "Cannot Sign In. Please Check the form and try again.. ", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+
 }
