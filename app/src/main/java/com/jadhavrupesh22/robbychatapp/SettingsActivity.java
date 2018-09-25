654dfbf0a34fe0b3424165e2ab4f41890db3f39b
Rupesh <jadhavrupesh22@gmail.com>
2018-09-25 22:55:47 +0530

    new ones

--- app/src/main/java/com/jadhavrupesh22/robbychatapp/SettingsActivity.java
+++ app/src/main/java/com/jadhavrupesh22/robbychatapp/SettingsActivity.java
@@ -91,4 +91,97 @@ public class SettingsActivity extends AppCompatActivity {
         });
 
     }
+
+    public void cs(View view) {
+        String statusValue = mStatus.getText().toString();
+
+
+        Intent status_intent = new Intent(SettingsActivity.this, StatusActivity.class);
+        status_intent.putExtra("status_value", statusValue);
+        startActivity(status_intent);
+
+    }
+
+    public void ci(View view) {
+//
+//        CropImage.activity()
+//                .setGuidelines(CropImageView.Guidelines.ON)
+//                .start(this);
+
+        Intent gallaryIntent = new Intent();
+        gallaryIntent.setType("images/*");
+        gallaryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(gallaryIntent, "Select Image"), GALLARY_PICK);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLARY_PICK && resultCode == RESULT_OK) {
+
+            Uri imageUrl = data.getData();
+            CropImage.activity(imageUrl)
+                    .setAspectRatio(1, 1)
+                    .start(this);
+
+            // Toast.makeText(SettingsActivity.this,imageUrl,Toast.LENGTH_LONG).show();
+        }
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+
+
+                mProgressDialog = new ProgressDialog(SettingsActivity.this);
+                mProgressDialog.setTitle("Uploading_Image");
+                mProgressDialog.setMessage("Wait while uploading image");
+                mProgressDialog.setCanceledOnTouchOutside(false);
+                mProgressDialog.show();
+
+                Uri resultUri = result.getUri();
+
+                String current_user_id = mCurrentuser.getUid();
+
+                StorageReference filepath = mImageStorage.child("profile_images").child(current_user_id + ".jpg");
+                filepath.putFile(resultUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(SettingsActivity.this, "Working...", Toast.LENGTH_LONG).show();
+
+                            String download_url = task.getResult().getMetadata().getReference().getDownloadUrl().toString();
+                            mUserDatabase.child("image").setValue(download_url).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+
+                                        Toast.makeText(SettingsActivity.this, "Added Successfully..", Toast.LENGTH_LONG).show();
+                                        mProgressDialog.dismiss();
+
+                                    }
+
+                                }
+                            });
+
+
+                        } else {
+
+                            Toast.makeText(SettingsActivity.this, "Error inUploading...", Toast.LENGTH_LONG).show();
+                            mProgressDialog.dismiss();
+                        }
+
+                    }
+                });
+
+
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/jadhavrupesh22/robbychatapp/StatusActivity.java
@@ -0,0 +1,81 @@
+package com.jadhavrupesh22.robbychatapp;
+
+
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class StatusActivity extends AppCompatActivity {
+    private android.support.v7.widget.Toolbar mToolbar;
+    private TextInputLayout mStatus;
+    private DatabaseReference mStatusDatabase;
+    private FirebaseUser mCurrentUser;
+
+
+    private ProgressDialog mProgress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_status);
+
+        mProgress = new ProgressDialog(this);
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String Current_uid = mCurrentUser.getUid();
+
+
+        mStatusDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(Current_uid);
+
+
+        mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.status_appBars);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Account Status");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mStatus = (TextInputLayout) findViewById(R.id.status_input);
+
+        String status_value = getIntent().getStringExtra("status_value");
+        mStatus.getEditText().setText(status_value);
+
+
+    }
+
+    public void ss(View view) {
+
+        mProgress.setTitle("Saving Changes");
+        mProgress.setMessage("Please Wait While Changes");
+        mProgress.show();
+
+        String status = mStatus.getEditText().getText().toString();
+
+        mStatusDatabase.child("status").setValue(status).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    mProgress.dismiss();
+                } else {
+                    Toast.makeText(StatusActivity.this, "something wrong", Toast.LENGTH_LONG).show();
+
+                }
+
+
+            }
+        });
+
+
+    }
+}
