3b7a781d198e2f826f205004d1ad3c3faa68e38b
Rupesh <jadhavrupesh22@gmail.com>
2018-09-28 16:23:37 +0530

    commite

--- app/src/main/java/com/jadhavrupesh22/robbychatapp/UsersActivity.java
+++ app/src/main/java/com/jadhavrupesh22/robbychatapp/UsersActivity.java
@@ -2,12 +2,30 @@ package com.jadhavrupesh22.robbychatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 public class UsersActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView mUserList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users);
+
+        //Toolbar
+        mToolbar=(android.support.v7.widget.Toolbar)findViewById(R.id.users_appBar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("All Users.");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        mUserList=(RecyclerView)findViewById(R.id.users_list);
+
+
+
+
     }
 }
