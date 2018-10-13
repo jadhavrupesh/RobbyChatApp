1fb13cf4f30e36f7e90c3e85f03bc4f5e7050af1
Rupesh <jadhavrupesh22@gmail.com>
2018-10-14 00:46:27 +0530

    Notification.....

--- app/src/main/java/com/jadhavrupesh22/robbychatapp/LoginActivity.java
+++ app/src/main/java/com/jadhavrupesh22/robbychatapp/LoginActivity.java
@@ -12,9 +12,13 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -29,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
     //progressBas
     private ProgressDialog mLoginProgress;
 
+    private DatabaseReference mUserDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +48,7 @@ public class LoginActivity extends AppCompatActivity {
 
         //Firebase
         mAuth = FirebaseAuth.getInstance();
+        mUserDatabase=FirebaseDatabase.getInstance().getReference().child("Users");
 
         //Progress
         mLoginProgress = new ProgressDialog(this);
@@ -80,10 +87,20 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (task.isSuccessful()) {
                     mLoginProgress.dismiss();
-                    Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
-                    mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                    startActivity(mainIntent);
-                    finish();
+
+                    String mCurrentUserId= mAuth.getCurrentUser().getUid();
+
+                    String deviceToken=FirebaseInstanceId.getInstance().getToken();
+                    mUserDatabase.child(mCurrentUserId).child("device_token").setValue(deviceToken).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(mainIntent);
+                            finish();
+                        }
+                    });
+
                 } else {
                     mLoginProgress.hide();
                     Toast.makeText(LoginActivity.this, "Invalid ID or password. Please try again.", Toast.LENGTH_LONG).show();
