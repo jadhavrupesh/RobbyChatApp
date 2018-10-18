a02f9b1f0cd59068904a1f5351d9fac408cf2fba
Rupesh <jadhavrupesh22@gmail.com>
2018-10-18 19:21:25 +0530

    User Online done

--- app/src/main/java/com/jadhavrupesh22/robbychatapp/FriendsFragment.java
+++ app/src/main/java/com/jadhavrupesh22/robbychatapp/FriendsFragment.java
@@ -115,6 +115,46 @@ public class FriendsFragment extends Fragment {
                         friendsViewHolder.setUserImage(userThumb,getContext());
 
 
+                        friendsViewHolder.mView.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+
+                                CharSequence options[] = new CharSequence[]{"Open Profile", "Send message"};
+
+                                final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                                builder.setTitle("Select Options");
+                                builder.setItems(options, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                        //Click Event for each item.
+                                        if(i == 0){
+
+                                            Intent profileIntent=new Intent(getContext(),ProfileActivity.class);
+                                            profileIntent.putExtra("user_Id",list_user_id);
+                                            startActivity(profileIntent);
+
+                                        }
+//
+//                                        if(i == 1){
+//
+//                                            Intent chatIntent = new Intent(getContext(), ChatActivity.class);
+//                                            chatIntent.putExtra("user_id", list_user_id);
+//                                            chatIntent.putExtra("user_name", userName);
+//                                            startActivity(chatIntent);
+//
+//                                        }
+
+                                    }
+                                });
+
+                                builder.show();
+
+                            }
+                        });
+
+
 
                     }
 
